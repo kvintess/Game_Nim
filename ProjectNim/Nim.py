a8256d5ae0224fbfbec54d6af5a9b29c569e5239
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 09:36:07 +0300

    5 first commit of 27th

--- ProjectNim/Nim.py
+++ ProjectNim/Nim.py
@@ -1,6 +1,6 @@
 #It's an easy mathematic game named Nim
 #it's my token, i'm afraid to forget it
-# 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
+# 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
 from Nim_engine import put_stones, take_from_bunch, get_bunches, is_gameover
 put_stones()
 while True:
