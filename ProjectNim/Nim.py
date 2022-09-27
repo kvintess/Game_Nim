d6bd84c8f61e97dcdc791bd34bee8e218fd87210
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 10:51:05 +0300

    7 used module termcolor to change color of input text and deleted token

--- ProjectNim/Nim.py
+++ ProjectNim/Nim.py
@@ -1,11 +1,11 @@
 #It's an easy mathematic game named Nim
-#it's my token, i'm afraid to forget it
-# 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
+
 from Nim_engine import put_stones, take_from_bunch, get_bunches, is_gameover
+from termcolor import cprint
 put_stones()
 user_number = 1
 while True:
-    print('Текущая позиция')
+    cprint('Текущая позиция', 'red'7)
     print(get_bunches())
     print('ходит игрок {}'.format(user_number))
     pos=int(input('Откуда берем?'))
