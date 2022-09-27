4d39053994d424d2d005ef2453458b30c837bbd0
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 10:36:24 +0300

    6 finished game for 2 players

--- ProjectNim/Nim.py
+++ ProjectNim/Nim.py
@@ -3,15 +3,15 @@
 # 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
 from Nim_engine import put_stones, take_from_bunch, get_bunches, is_gameover
 put_stones()
+user_number = 1
 while True:
     print('Текущая позиция')
     print(get_bunches())
+    print('ходит игрок {}'.format(user_number))
     pos=int(input('Откуда берем?'))
     qua=int(input('Сколько берем?'))
     take_from_bunch(position=pos, quantity=qua)
-    print(len(Nim_engine._holder))
-
-
-
     if is_gameover():
         break
+    user_number = 2 if user_number == 1 else 1
+print('победа игрока номер ', user_number)
